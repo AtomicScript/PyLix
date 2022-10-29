9b44ae1a35aba2d41151e9d54e4dfe7f8906449e
Atom <bananapredict@gmail.com>
2022-10-29 21:46:01 +0400

    solved challenge 5

--- cryptography/challenges/5.py
+++ cryptography/challenges/5.py
@@ -1,13 +1,22 @@
 import hashlib
 
 hash = 'c09145ad46b058fba82e4218169c7121'
-def firstPart():
-    p = 'password'
+p = 'password'
+found = False
+
+def firstPart(p):
+    global found
     h = p
-    # password is hashed 10 times
-    for i in range(10):
-        h = hashlib.md5(h.encode('utf-8')).hexdigest()
-        print(i+1, h)
+    if found == False:
+        # password is hashed 10 times
+        for i in range(100):
+            h = hashlib.md5(str(h).encode('utf-8')).hexdigest()
+            if h == hash:
+                print("HASH HAS BEEN FOUND")
+                print(f'Password : {p}, Hash : {h}')
+                found = True
+                break
+            print(p, i+1, h)
 
 def hash100(h):
     h = h
@@ -18,15 +27,15 @@ def hash100(h):
         if h == hash:
             print("Hash found")
 
-# WORK ON IT LATER 
+# WORK ON IT LATER
 def secondPart():
     # 3-digit password hashed 100 times with MD5. Find it from this hash
     # password will be hashed 100 times
     # for each number it will be hashed 100 times
+    # Password : 373, Hash : c09145ad46b058fba82e4218169c7121
     for x in range(1000):
-        for y in range(101):
-            h = hashlib.md5(str(x).encode('utf-8')).hexdigest()
-            print(x, y, h)
+        if not found:
+            firstPart(x)
 
 
 secondPart()
